a3fc84ced06511b3c4b2199443cba36448e13e16
rytrsti0001k <rytr.sti0001k@gmail.com>
2026-02-25 23:39:45 +0900

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def test():
-    return f"API_KEY={os.environ.get('FINNHUB_API_KEY')}"
+API_KEY = os.environ.get("TWELVE_API_KEY")
+
+@app.route("/stock/<symbol>")
+def get_stock(symbol):
+
+    symbol_t = f"{symbol}.T"
+
+    # 株価取得
+    price_url = f"https://api.twelvedata.com/price?symbol={symbol_t}&apikey={API_KEY}"
+    price_res = requests.get(price_url).json()
+
+    # RSI取得
+    rsi_url = f"https://api.twelvedata.com/rsi?symbol={symbol_t}&interval=1day&time_period=14&apikey={API_KEY}"
+    rsi_res = requests.get(rsi_url).json()
+
+    if "price" not in price_res or "values" not in rsi_res:
+        return jsonify({"error": "API error", "price": price_res, "rsi": rsi_res}), 400
+
+    return jsonify({
+        "price": float(price_res["price"]),
+        "RSI": float(rsi_res["values"][0]["rsi"])
+    })
